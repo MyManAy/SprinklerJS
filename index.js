2a7b63954384b1b0585ab0178856853a56c5600b
MyManAy <66086069+MyManAy@users.noreply.github.com>
2021-02-21 00:20:52 -0600

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log("we in this bitch");
+});
+client.on("message", message => {
+    if (!!message.author.bot) return;
+    yoAssCaught = message.content.match(/(r+\s*)+(i+\s*)+(n+\s*)+/gi);
+    if (yoAssCaught !== null) {
+        message.delete();
+        message.channel.send(`${message.author.username}'s message was deleted for containing '${yoAssCaught.join('')}'`);
+    }
+});
+
+client.login("ODEyOTIzODUwNjAxNzkxNTI5.YDH0VQ.iyHBPYchdoF6pYrWFzPKdwnAArI");
+
