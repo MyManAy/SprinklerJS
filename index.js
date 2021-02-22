de55e4b52716aaad46d3eabf7f8a6c845738e11c
MyManAy <66086069+MyManAy@users.noreply.github.com>
2021-02-21 18:36:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ client.once('ready', () => {
 });
 
 
-const rRegex = /(r+[\W\s_]*)+(?=((i+[\W\s_]*)+((n+[\W\s_]*)+)?)?$)/gi;
-const iRegex = /(i+[\W\s_]*)+(?=((n+[\W\s_]*)+)?$)/gi;
+const rRegex = /(r+[\W\s_]*)+(?=(([li]+[\W\s_]*)+((n+[\W\s_]*)+)?)?$)/gi;
+const iRegex = /([li]+[\W\s_]*)+(?=((n+[\W\s_]*)+)?$)/gi;
 const nRegex = /(n+[\W\s_]*)+$/gi;
 const regexRecurs = [rRegex, iRegex, nRegex];
 var count = 0;
@@ -18,8 +18,8 @@ client.on("message", message => {
     if (!!message.author.bot) return;
     function rinAlg(num) {
         let match = message.content.match(regexRecurs[num]);
-        let safeAttempt = message.content.match(/[rin]$/gi);
-        let pure = message.content.match(/[\W\s_]+(r+[\W\s_]*)+(i+[\W\s_]*)+(n+[\W\s_]*)+[\W\s_]+/gi)
+        let safeAttempt = message.content.match(/[riln]$/gi);
+        let pure = message.content.match(/[\W\s_]+(r+[\W\s_]*)+([il]+[\W\s_]*)+(n+[\W\s_]*)+[\W\s_]+/gi)
         if (num < 3) {
             if (pure !== null) {
                 message.channel.send(`warning: consecutive messages containing "${pure.join('').trim()}"`);
