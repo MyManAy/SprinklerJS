a1e39fd8c77c425469415098d180970f62049d96
MyManAy <66086069+MyManAy@users.noreply.github.com>
2021-02-21 20:03:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on("message", message => {
     if (!!message.author.bot) return;
     function rinAlg(num) {
         let match = message.content.match(regexRecurs[num]);
-        let safeAttempt = message.content.match(/[riln]$/gi);
+        let safeAttempt = message.content.match(/[riln\W\s_]$/gi);
         let pure = message.content.match(/[\W\s_]+(r+[\W\s_]*)+([il]+[\W\s_]*)+(n+[\W\s_]*)+[\W\s_]+/gi)
         if (num < 3) {
             if (pure !== null) {
