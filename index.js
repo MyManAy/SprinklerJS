ce4820e914df852b798237cd69de5169bda8d5e2
MyManAy <66086069+MyManAy@users.noreply.github.com>
2021-03-14 15:45:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ ${current_status}
             );
         } else if (command === "guess") {
             if (message.author.id in hangman_games) {
-                send_to_channel(message.channel.id, args)
+                send_to_channel(message.channel.id, typeof(args));
                 guess = args[0];
                 if (guess.match(/[a-z]/i) !== null) {
                     if (hangman_games[message.author.id]["complete_word"].includes(guess)) {
